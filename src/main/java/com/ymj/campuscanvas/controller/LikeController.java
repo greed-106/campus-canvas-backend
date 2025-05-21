dc61a4dd95739bd6eeeeaf7bb2482f57979a3eb3
greed-106 <ymj20040312@gmail.com>
2025-05-21 18:46:38 +0800

    完善了点赞的添加与删除功能

--- src/main/java/com/ymj/campuscanvas/controller/LikeController.java
+++ src/main/java/com/ymj/campuscanvas/controller/LikeController.java
@@ -1,4 +1,25 @@
 package com.ymj.campuscanvas.controller;
 
+import com.ymj.campuscanvas.pojo.DTO.LikeRequest;
+import com.ymj.campuscanvas.pojo.Result;
+import com.ymj.campuscanvas.service.LikeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+@CrossOrigin
+@RequestMapping("/campus-canvas/api/likes")
 public class LikeController {
+    @Autowired
+    LikeService likeService;
+
+    @PostMapping
+    public Result handleLikeRequest(@RequestBody LikeRequest likeRequest) {
+        log.info("Received like request: {}", likeRequest);
+        likeService.handleLikeRequest(likeRequest);
+        return Result.success();
+    }
 }
+
--- src/main/java/com/ymj/campuscanvas/mapper/PostLikeMapper.java
+++ src/main/java/com/ymj/campuscanvas/mapper/PostLikeMapper.java
@@ -9,13 +9,13 @@ import java.util.List;
 public interface PostLikeMapper {
     @Insert("INSERT INTO `like_post`(user_id, target_id) VALUES(#{userId}, #{targetId})")
     @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
-    void insertLike(Like like);
+    void insertPostLike(Like like);
     @Select("SELECT COUNT(*) FROM `like_post` WHERE target_id = #{postId}")
-    int countLikesByPostId(Long postId);
+    int countPostLikesByPostId(Long postId);
     @Select("SELECT COUNT(*) > 0 FROM `like_post` WHERE user_id = #{userId} AND target_id = #{targetId}")
     boolean isLikedByUser(Like like);
     @Delete("DELETE FROM `like_post` WHERE user_id = #{userId} AND target_id = #{targetId}")
-    void deleteLike(Like like);
+    int deleteLike(Like like);
     @Select("SELECT * FROM `like_post` WHERE user_id = #{userId}")
     List<Like> getLikesByUserId(Long userId);
     @Select("SELECT * FROM `like_post` WHERE target_id = #{postId}")
