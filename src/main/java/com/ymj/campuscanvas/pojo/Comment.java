a6156bb53f8515a7cde115901d4c956379c9bbdb
greed-106 <ymj20040312@gmail.com>
2025-05-22 22:44:05 +0800

    添加了向DTO转换的方法

--- src/main/java/com/ymj/campuscanvas/pojo/Comment.java
+++ src/main/java/com/ymj/campuscanvas/pojo/Comment.java
@@ -1,5 +1,6 @@
 package com.ymj.campuscanvas.pojo;
 
+import com.ymj.campuscanvas.pojo.DTO.GetCommentResponse;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,4 +16,17 @@ public class Comment {
     private Long postId;
     private String content;
     private LocalDateTime createdTime;
+
+    public GetCommentResponse toGetCommentResponse() {
+        return new GetCommentResponse(
+                this.id,
+                this.userId,
+                null, // userName will be set in the service layer
+                null, // userAvatar will be set in the service layer
+                this.postId,
+                this.content,
+                this.createdTime,
+                0 // likes will be set in the service layer
+        );
+    }
 }
